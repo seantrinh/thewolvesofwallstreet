f34833456a75014c3c9c0f2e6f264d25f1bf10d5
Sean Trinh <strinh@stevens.edu>
2019-02-19 15:03:55 -0500

    add main

--- code/main.py
+++ code/main.py
@@ -1,7 +1,26 @@
 # Aaron John, Sean Trinh, Hariharan Vijayachandran
 
-def main():
+import shift
+import sys
+import time
+def main(argv):
     print("hello world")
+    # create trader object
+    trader = shift.Trader("test001") #Change this?
+
+    # connect and subscribe to all available order books
+    try:
+        trader.connect("initiator.cfg", "password")
+        trader.subAllOrderBook()
+    except shift.IncorrectPassword as e:
+        print(e)
+    except shift.ConnectionTimeout as e:
+        print(e)
+
+    #Execute methods
+
+    #Disconnect
+    trader.disconnect()
 
 if __name__ == "__main__":
     main(sys.argv)
